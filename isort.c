b30d5967118d8c97e23c821ea6ce431227232545
Roy <me@hiyorix.com>
2022-12-19 22:44:31 +0200

    Uploading stracture

---
+++ isort.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define ARR_SIZE 50
+
+void shift_element(int* arr, int i);
+
+void insertion_sort(int* arr, int len);
+
+int main() {
+    return 0;
+}
---
+++ txtfind.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define LINE 256
+#define WORD 30
+
+int getline(char s[]);
+int getword(char w[]);
+int substring(char * str1, char * str2);
+int similar (char *s, char *t, int n);
+void print_lines(char * str);
+void print_similar_words(char * str);
+
+int main() {
+    return 0;
+}
